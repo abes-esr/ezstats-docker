99b905cdd42edd2ae5ce19090b6eed3a3af53b81
Aurélien Charot <charot@abes.fr>
2024-06-05 19:20:00 +0200

    Correction logs batch

--- batch/docker-entrypoint.sh
+++ batch/docker-entrypoint.sh
@@ -2,11 +2,11 @@
 
 { echo ""; \
   echo "# EZP Bulk"; \
-  echo "* * * * * /home/node/launch-ezp.sh >> /home/node/launch-ezp.log 2>&1"; \
+  echo "* * * * * /home/node/launch-ezp.sh 1>/proc/1/fd/1 2>/proc/1/fd/2"; \
 	} | crontab -
 
 # start cron
-echo "Starting cron..."
+echo "Starting EZP bulk cron..."
 service cron start
 
 exec "$@"
