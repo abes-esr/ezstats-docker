4bae72c85671c41c7f5f3a237fff108f11aab67b
Buddy-Lee CELDRAN <44428671+BudNatsu@users.noreply.github.com>
2024-06-28 11:05:01 +0200

    Update zip.sh

--- batch-logs/zip.sh
+++ batch-logs/zip.sh
@@ -2,8 +2,13 @@
 ##chemin vers le point de montage ezstats-logs:/log
 
 RESULT_DIR="/home/node/logtheses/logs/2024"
+#RESULT_DIR="/home/node/logtheses/logs/$(date +%Y)/$(date +%m)"
 mkdir -p "$RESULT_DIR"
 
+#for Logdir in $(ls /home/node/logstash/*/|cut -d ' ' -f 1 )
+#do
+#         mkdir -p "$RESULT_DIR/${Logdir}"
+#done
 
 cd ${RESULT_DIR}
 for LOG_FILE in $(ls /home/node/logstash/*/*/logstash-* | grep -vE "\.raw\.log$")
@@ -12,7 +17,8 @@ do
                 if [ ! -f ${LOG_FILE}.raw.log ]; then
                         cat ${LOG_FILE} | \
                         jq -r 'select(.container.name == "theses-rp") | .event.original' | \
-                        grep -v -E "^20[0-9]{2}-[0-9]{2}-[0-9]{2}" \
+                        grep -v -E "^20[0-9]{2}-[0-9]{2}-[0-9]{2}" | \
+                        sed -E 's/([0-9]{1,3}\.[0-9]{1,3})\.[0-9]{1,3}\.[0-9]{1,3}/\1.0.0/g' \
                         > "${RESULT_DIR}/${Logresult}.raw.log"
                 fi
 
