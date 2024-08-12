3287385ccfeaa2b920739fd6465541b5fe3836b1
LAP-TMX-VIRT\thomas <michaux@abes.fr>
2024-08-12 17:13:34 +0200

    relax pattern on customs vars (Referer, Schibboleth) to fix matching of authenticated urls (/api/v1/document/protected/ )

--- batch-theses/launch-ezp.sh
+++ batch-theses/launch-ezp.sh
@@ -9,5 +9,5 @@ ezp bulk -r -v logtheses/data/thesesfr/logs/ logtheses/data/thesesfr/results/ \
                 -H "filter-platforms: thesesfr" \
                 -H "ezPAARSE-Middlewares: thesesfr" \
                 -H "Output-Fields: +nnt, +numSujet, +etabSoutenanceN, +etabSoutenancePpn, +dateSoutenance, +dateInscription, +statut, +accessible, +source, +discipline, +domaine, +langue, +ecoleDoctoraleN, +ecoleDoctoralePpn, +partenaireRechercheN, +partenaireRecherchePpn, +cotutelleN, +cotutellePpn, +auteurN, +auteurPpn, +directeurN, +directeurPpn, +presidentN, +presidentPpn, +rapporteursN, +rapporteursPpn, +membresN, +membresPpn, +personneN, +personnePpn, +organismeN, +organismePpn" \
-                -H "Log-Format-apache: %h %l %u %t \"%r\" %>s %b \"%{Referer}i\" \"%{User-Agent}<.*>\" \"%{Shib-Identity-Provider}i\" \"%{eppn}i\" \"%{primary-affiliation}i\" \"%{supannEtablissement}i\""
-
+				-H "Log-Format-apache: %h %l %u %t \"%r\" %>s %b \"%{Referer}<.*>\" \"%{User-Agent}<.*>\" \"%{Shib-Identity-Provider}<.*>\" \"%{eppn}<.*>\" \"%{primary-affiliation}<.*>\" \"%{supannEtablissement}<.*>\" " \
+				
