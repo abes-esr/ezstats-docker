e117c0bad2df92783a6d719fffe66ba90005c944
Aurélien Charot <charot@abes.fr>
2024-12-13 08:57:24 +0100

    Version de William avec écriture sur disque

--- ezpaarse-init/middlewares/idp-metadata/index.js
+++ ezpaarse-init/middlewares/idp-metadata/index.js
@@ -1,127 +1,161 @@
-'use strict';
+"use strict";
 
-const co = require('co');
-const request = require('request');
-const { bufferedProcess, wait } = require('../utils.js');
-const xmlMapping = require('xml-mapping')
+const request = require("request");
+const { bufferedProcess, wait } = require("../utils.js");
+const xmlMapping = require("xml-mapping");
+const path = require("path");
+const fs = require("fs");
 
-const oneDay = 24 * 60 * 60 * 1000;
-let lastRefresh = Date.now();
-let list_idp;
-
-module.exports = function () {
+module.exports = async function () {
     const logger = this.logger;
     const report = this.report;
     const req = this.request;
 
-    logger.info('Initializing ABES idp-metadata middleware');
+    logger.info("Initializing ABES idp-metadata middleware");
 
     // Maximum number of Theses or Persons to query
-    let packetSize = parseInt(req.header('idp-metadata-packet-size'));
+    const packetSize = parseInt(req.header("idp-metadata-packet-size")) || 100; // Default: 100
     // Minimum number of ECs to keep before resolving them
-    let bufferSize = parseInt(req.header('idp-metadata-buffer-size'));
-    if (isNaN(packetSize)) { packetSize = 100; } //Default : 50
-    if (isNaN(bufferSize)) { bufferSize = 1000; } //Default : 1000
+    const bufferSize = parseInt(req.header("idp-metadata-buffer-size")) || 1000; // Default: 1000
 
-    report.set('idp-metadata', 'idp-metadata-queries', 0);
-    report.set('idp-metadata', 'idp-metadata-query-fails', 0);
-    report.set('idp-metadata', 'idp-metadata-cache-fails', 0);
+    //Mise en place du cache
+    const cacheFileName = "idp-metadata.json";
+    const cacheFilePath = path.resolve(__dirname, cacheFileName);
+    const cacheRefreshTime =
+        parseInt(req.header("idp-metadata-cache-refresh-time")) || 60 * 60 * 24; // Default: 1 day
 
-    const process = bufferedProcess(this, {
-        packetSize,
-        bufferSize,
-        /**
-         * Filter ECs that should be enriched
-         * @param {Object} ec
-         * @returns {Boolean|Promise} true if the EC as an unitid, false otherwise
-         */
-        filter: ec => !!ec?.unitid,
-        onPacket: co.wrap(onPacket)
-    });
+    logger.info(`Cache refresh set at ${cacheRefreshTime} seconds.`);
 
-    //Chargement du mapping par fichier (list_idp.xml)
-    function chargeMapping(nomFichier, resolve, reject){
-        fs.readFile(path.resolve(__dirname, nomFichier), 'utf8', (err, content) => {
-            if (err) {
-                return reject(err);
-            }
+    report.set("idp-metadata", "idp-metadata-queries", 0);
+    report.set("idp-metadata", "idp-metadata-query-fails", 0);
+    report.set("idp-metadata", "idp-metadata-cache-fails", 0);
 
-            try {
-                logger.info('Erreur chargement du mapping par web service : chargement par le fichier '+nomFichier+' OK');
-                return resolve(content);
-            } catch (e) {
-                return reject(e);
-            }
-        });
+    /**
+     * Try to read the content of a JSON file
+     * @param {string} filePath
+     * @returns
+     */
+    function getJSONFileContent(filePath) {
+        try {
+            const buffer = fs.readFileSync(filePath, { encodage: "utf8" });
+            const content = buffer.toString("utf-8");
+            return JSON.parse(content);
+        } catch (error) {
+            logger.error(`Erreur au chargement du fichier ${filePath}`);
+            throw error;
+        }
     }
 
+    /**
+     * Try to write new content in a JSON file,
+     * otherwise restore the old content
+     * @param {string} filePath
+     * @param {Object} content
+     * @returns
+     */
+    function setJSONFileContent(filePath, content) {
+        const oldContent = getJSONFileContent(filePath);
+        try {
+            return fs.writeFileSync(filePath, JSON.stringify(content));
+        } catch (error) {
+            fs.writeFileSync(filePath, JSON.stringify(oldContent));
+            logger.error(`Erreur à l'écriture du fichier ${filePath}`);
+            throw error;
+        }
+    }
 
+    /**
+     * Returns if a file was modified more than a day ago.
+     * @param {string} filePath
+     * @returns
+     */
+    function isFileFresh(filePath) {
+        try {
+            const fileStats = fs.statSync(filePath);
+            const lastModifiedDate = fileStats.mtime;
+            const lastModifiedAgo = (new Date().getTime() - lastModifiedDate) / 1000; // in seconds
+            const isFresh = lastModifiedAgo <= cacheRefreshTime;
+            logger.info(
+                `File updated ${lastModifiedAgo} seconds ago. File ${
+                    isFresh ? "valid" : "invalid"
+                }.`
+            );
+            return isFresh; // 24 hours
+        } catch {
+            return false;
+        }
+    }
 
-    const promiseIdP = new Promise((resolveIdP, rejectIdP) => {
-
-        if (list_idp && ((Date.now() - lastRefresh) < oneDay)) { return resolveIdP(list_idp); }
-
-        logger.info('Rafraichissement du mapping : list_idp');
-
-        //Chargement du mapping par appel au web service Renater
-        const optionsIdP = {
-            method: 'GET',
-            uri: `https://pub.federation.renater.fr/metadata/renater/main/main-idps-renater-metadata.xml`
+    /**
+     * Returns the XML datas from the IDP Renater
+     * using cache or API
+     */
+    async function getIDPRenaterMetadata() {
+        if (isFileFresh(cacheFilePath)) {
+            logger.info("Récupération du cache des données de Renater.");
+            return getJSONFileContent(cacheFilePath);
+        }
+        const requestConfig = {
+            method: "GET",
+            uri: `https://pub.federation.renater.fr/metadata/renater/main/main-idps-renater-metadata.xml`,
         };
 
-        request(optionsIdP, (errIdP, responseIdP, resultIdP) => {
-            //Si erreur, chargement du fichier list_idp.xml, a la place
-            if (errIdP || responseIdP.statusCode !== 200) {
-                chargeMapping('list_idp.xml', resolveIdP, rejectIdP);
-            };
+        logger.info("Récupération des données Renater par l'API.");
 
-            lastRefresh = Date.now();
-            resolveIdP(resultIdP)
-        });
-    });
-
-    return new Promise(function (resolve, reject) {
-        Promise.all([promiseIdP])
-            .then((promises) => {
-                list_idp = xmlMapping.tojson(promises[0]);
-                resolve(process);
-            })
-            .catch(function(err) {
-                logger.error(`idp-metadata: erreur chargement des mappings : ${err}`);
-                return reject(new Error('idp-metadata: erreur chargement des mappings'));
+        return new Promise((resolve) => {
+            request(requestConfig, (err, response, result) => {
+                if (err || response.statusCode !== 200) {
+                    logger.error("Erreur de récupération par l'API Renater.");
+                    logger.info("Récupération du cache des données de Renater.");
+                    resolve(getJSONFileContent(cacheFilePath));
+                }
+                try {
+                    logger.info("Mise à jour des données en cache de Renater.");
+                    setJSONFileContent(cacheFilePath, xmlMapping.tojson(result));
+                } catch (e) {
+                    logger.error(e.message);
+                } finally {
+                    logger.info("Récupération du cache des données de Renater.");
+                    resolve(getJSONFileContent(cacheFilePath));
+                }
             });
-    });
-
-    /**
-     * Process a packet of ECs
-     * @param {Array<Object>} ecs
-     * @param {Map<String, Set<String>>} groups
-     */
-    function* onPacket({ ecs }) {
-        if (ecs.length === 0) { return; }
-        for (const [ec, done] of ecs) {
-            enrichEc(ec)
-            done();
-        }
+        });
     }
-    /**
-     * Enrich an EC using the result of a query
-     * @param {Object} ec the EC to be enriched
-     * @param {Object} result the document used to enrich the EC
-     */
 
-    /* ERM header cible
- 	# -H "Output-Fields: +nnt, +numSujet, +doiThese, +etabSoutenanceN, +etabSoutenancePpn, +codeCourt, +dateSoutenance, +anneeSoutenance, +dateInscription, +anneeInscription, +statut, +accessible, +source, +discipline, +domaine, +langue, +ecoleDoctoraleN, +ecoleDoctoralePpn, +partenaireRechercheN, +partenaireRecherchePpn, +cotutelleN, +cotutellePpn, +auteurN, +auteurPpn, +directeurN, +directeurPpn, +presidentN, +presidentPpn, +rapporteursN, +rapporteursPpn, +membresN, +membresPpn, +personneN, +personnePpn, +organismeN, +organismePpn, +idp_etab_nom, +idp_etab_ppn, +idp_etab_code_court, +platform_name " \
-    */
+    const idpMetadata = await getIDPRenaterMetadata();
+
+    /**
+     * Enrich the current EC with the list_idp
+     * @param {Object} ec
+     */
     function enrichEc(ec) {
-        if(ec['Shib-Identity-Provider']) {
-            logger.info(`Tentative de recherche du libellé d'IDP ${ec['Shib-Identity-Provider']} pour l'EC ${ec.unitid}`);
-            const etab = list_idp.md$EntitiesDescriptor.md$EntityDescriptor.find((entityDescriptor) => entityDescriptor.entityID === ec['Shib-Identity-Provider'])
-            const info = etab.md$IDPSSODescriptor.md$Extensions.mdui$UIInfo
-            ec.libelle_idp = info.mdui$DisplayName.find((displayName) => displayName.xml$lang === "fr")?.$t;
+        if (ec["Shib-Identity-Provider"]) {
+            logger.info(
+                `Tentative de recherche du libellé d'IDP ${ec["Shib-Identity-Provider"]} pour l'EC ${ec.unitid}`
+            );
+            const etab = idpMetadata.md$EntitiesDescriptor.md$EntityDescriptor.find(
+                (entityDescriptor) =>
+                    entityDescriptor.entityID === ec["Shib-Identity-Provider"]
+            );
+            const info = etab.md$IDPSSODescriptor.md$Extensions.mdui$UIInfo;
+            ec.libelle_idp = info.mdui$DisplayName.find(
+                (displayName) => displayName.xml$lang === "fr"
+            )?.$t;
         }
         if (!ec.libelle_idp) {
-            ec.libelle_idp = "sans objet"
+            ec.libelle_idp = "sans objet";
         }
     }
+
+    return bufferedProcess(this, {
+        packetSize,
+        bufferSize,
+        filter: (ec) => !!ec?.unitid,
+        onPacket: ({ ecs }) => {
+            for (const [ec, done] of ecs) {
+                enrichEc(ec);
+                done();
+            }
+        },
+    });
 };
